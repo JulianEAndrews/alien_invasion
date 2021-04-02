63f05ad7e10a83a908dd36a6b03d795a9f754f4c
Julian E Andrews <juliane.andrews@gmail.com>
2021-04-01 20:56:57 -0700

    Fire the bullets!

--- settings.py
+++ settings.py
@@ -14,3 +14,10 @@ class Settings:
 
         # Ship Settings
         self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 95, 31) # Neon orange
+        self.bullets_allowed = 3
