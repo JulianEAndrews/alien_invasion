fce033c88c85b7e30f62b8b612f47641a48d3c76
Julian E Andrews <juliane.andrews@gmail.com>
2021-04-02 09:14:47 -0700

    First alien drawn to screen. Next: create fleet and add movement.

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,6 +15,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     '''Overall class to manage game assets and behavior.'''
@@ -36,6 +37,10 @@ class AlienInvasion:
         self.ship = Ship(self)
 
         self.bullets = pygame.sprite.Group()
+
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         '''Start the main loop for the game.'''
@@ -87,6 +92,12 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        '''Create the fleet of aliens'''
+        # Make and alien
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     def _update_screen(self):
         '''Update images on the screen, and flip to the new screen.'''
         self.screen.fill(self.settings.bg_color) #To use bg color
@@ -95,6 +106,8 @@ class AlienInvasion:
 
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visible
         pygame.display.flip()
