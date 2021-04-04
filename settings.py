2bf4cb6d285e831d024dba3deb15c7d44c0334ef
Julian E Andrews <juliane.andrews@gmail.com>
2021-04-04 09:27:38 -0700

    Added +x fleet movement.

--- settings.py
+++ settings.py
@@ -21,3 +21,6 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (255, 95, 31) # Neon orange
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
