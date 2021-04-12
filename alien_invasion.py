9faf3994c179299ca1a9fee5f8626f8f391baee9
Julian E Andrews <juliane.andrews@gmail.com>
2021-04-12 15:06:23 -0700

    Cleaned up game start interface, can press 'p' to start, can press 'r' to reset during debugging.

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,32 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        '''Start a new game when the player clicks Play'''
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            if self.play_button.rect.collidepoint(mouse_pos):
+                self._start_game()
+
+    def _start_game(self):
+        # Reset game statistics.
+        self.stats.reset_stats()
+        self.stats.game_active = True
+
+        # Get rid of any remaining aliens and bullets.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Hide the mouse cursor.
+        pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         '''Respond to keypresses.'''
@@ -83,8 +109,13 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_r:
+            self._start_game()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        elif event.key == pygame.K_p:
+            if not self.stats.game_active:
+                self._start_game()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -162,6 +193,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _create_fleet(self):
         '''Create the fleet of aliens'''
--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
 
         # Ship Settings
         self.ship_speed = 1.5
-        self.ship_limit = 3
+        self.ship_limit = 2
 
         # Bullet settings
         self.bullet_speed = 200
@@ -31,6 +31,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10 # Change back to 10 for regular gameplay
+        self.fleet_drop_speed = 100 # Change back to 10 for regular gameplay
         # Fleet direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
